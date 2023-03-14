e8783c652040c96deaa1b69353fbeecf498f0b50
Imanda03 <asis03ktm@gmail.com>
2023-03-14 22:27:59 +0545

    data_form_url

---
+++ URL_Class/DataFormURL.java
@@ -0,0 +1,24 @@
+package URL_Class;
+
+import java.net.*;
+import java.io.*;
+// import java.util.Scanner;
+
+public class DataFormURL {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        try {
+
+            // URL url = new URL(sc.nextLine());
+            URLConnection ucon = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(ucon.getInputStream()));
+            String line;
+            // sc.close();
+            while ((line = br.readLine()) != null) {
+                System.out.println(line + "\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Check your code" + e);
+        }
+    }
+}
---
+++ URL_Class/DataFormURLUSer.java
@@ -0,0 +1,36 @@
+package URL_Class;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class DataFormURLUSer {
+    public static String printData(String text) throws IOException {
+        URL url = new URL(text);
+        URLConnection ucon = url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(ucon.getInputStream()));
+        String line;
+        String data = "";
+        while ((line = br.readLine()) != null) {
+            data += line;
+        }
+
+        return data;
+
+    }
+
+}
+
+class Program {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            String text = sc.nextLine();
+            String content = DataFormURLUSer.printData(text);
+            System.out.println(content);
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("Check your code" + e);
+        }
+    }
+}
